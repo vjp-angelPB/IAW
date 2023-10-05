dbca1effdf2e7e37112ec1ced3116bfd704d57fb
2º ASIR - Ángel Pérez Blanco <aperezb33@informatica.iesvalledeljerteplasencia.es>
2023-10-05 20:58:19 +0200

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,14 +1,24 @@
-function CalculadoraCientifica(){
-    var nombre="";
-    var altura=1.99;
-    
+/*Creo la función para cambiar a la calculadora científica y que se muestre el botón "elevar" */
+function cambiarcientifico(){
+    let accionelevar = document.getElementById('botonelevar');
+    accionelevar.disabled=true;
+};
+
+/*Creo la función para cambiar a la calculadora normal y que se oculte el botón "elevar" */
+function cambiarnormal(){
+    let accionocultar = document.getElementById('botonelevar')
+    accionocultar.disabled=false;
+};
+
+
+/*Creo la función que hace que se pongan por defecto el valor 0 en todos los input para 
+volver a introducir nuevos datos */
+function Restaurar(){
+    if (document.getElementById("textoverde")!=null){
+        document.getElementById("seccion-operaciones-id")
     }
-    
+};
 
+function Lupa(){
 
-
-
-
-
-
-
+};
